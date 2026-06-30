10c2cf46ffeb71699b2ba6cd120f2f3bcaa52a7a
nishants45 <nishants7049@gmail.com>
2026-06-30 22:30:07 +0530

    this is my first javascript class

---
+++ Basic_tutorial/variable.js
@@ -0,0 +1,24 @@
+console.log("Nishant singh");   //it is used as printf means to priint something on screen
+
+// To create Variable
+const accountid = 738622    //const variable cannot change
+let accountemail = "nishant@gmail.com" 
+var accountsate = "Uttarpradesh"
+accountpasssword = 12234      // in javascript we can declare variable directly but it is suggested to use this method
+let accountcity;           //is case me undefined print hoga 
+
+
+//const,let,var are keyword in js
+// declaring a variable has many ways like by using let,var , or without using any keyword 
+//but we use generally let because it is latest and most use
+
+ 
+console.log(accountid);
+
+console.table([accountid,accountemail,accountsate,accountpasssword]);      // console table is used to print all in a table 
+// together in this we use[] this bracket for writing info//
+
+ 
+
+{}    //ham isko scope kehte hii dhyaan rkhna
+
