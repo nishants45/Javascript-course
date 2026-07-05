acb2714cfd3354bf9cbbc855701974b84e40938d
nishants45 <nishants7049@gmail.com>
2026-07-05 21:32:41 +0530

    completed data types lecture

---
+++ Basic_tutorial/data_types.js
@@ -0,0 +1,39 @@
+"use strict";   //treat all javascript code as newer version
+
+// alert( 3+3)  // we are using nodejs not browser
+
+
+console.log(3+3);
+
+//for seeing documentation us mdn site 
+
+//=========================================Data types============================//
+
+let name = "NISHNAT"      //string datatypes generally in double quotes
+let age = 18             //number datatypes without quotes
+let islogin = false       // boolean datatypes
+// these are some example
+
+//===============types of data types in javascript=================//
+/*
+  1.Number datatypes => 2 to power 53
+  2.bigint -- generally use for big    no.
+  3.String datatypes  --prefer writing it in double quote
+  4. Boolean datatypes =>true/false
+  5. null -- it is standalone value   (empty)
+  6. Undefined => means we have not define the value but we had declare its datatypes
+  7. Symbol datatypes =for uniqueness
+  
+
+  8.object = learn later
+*/
+
+
+console.log(typeof false);      //by this you can know type of data
+
+console.log(typeof undefined);   //output--undefined
+console.log(typeof null);      //output-- object
+
+
+
+
--- Basic_tutorial/variable.js
+++ Basic_tutorial/variable.js
@@ -1,22 +1,27 @@
 console.log("Nishant singh");   //it is used as printf means to priint something on screen
+console.log("This is first video of javascript");
 
 // To create Variable
 const accountid = 738622    //const variable cannot change
 let accountemail = "nishant@gmail.com" 
 var accountsate = "Uttarpradesh"
-accountpasssword = 12234      // in javascript we can declare variable directly but it is suggested to use this method
+accountpasssword = "12234"      // in javascript we can declare variable directly but it is suggested to use this method
 let accountcity;           //is case me undefined print hoga 
 
 
-//const,let,var are keyword in js
 // declaring a variable has many ways like by using let,var , or without using any keyword 
-//but we use generally let because it is latest and most use
 
- 
+ /*
+ prefer not to use var because of isssue in block scope and funtional scope
+ rather use "Let" for declaring varriable 
+ */
 console.log(accountid);
 
-console.table([accountid,accountemail,accountsate,accountpasssword]);      // console table is used to print all in a table 
+console.table([accountid,accountemail,accountsate,accountpasssword,accountcity]);      // console table is used to print all in a table 
 // together in this we use[] this bracket for writing info//
+
+let username = "nishant@gmail.com"
+console.log(username);
 
  
 
