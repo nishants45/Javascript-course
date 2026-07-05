b86a1068d6f8f942d24262f5f84d863823fe4d45
nishants45 <nishants7049@gmail.com>
2026-07-05 22:36:48 +0530

    today completed conversion topic

---
+++ Basic_tutorial/conversionoroperation.js
@@ -0,0 +1,63 @@
+// let score = 33
+
+// console.log(typeof score);
+
+
+let score = "33abc"
+
+console.log(typeof score);    // yha pe output string aaya
+
+//================conversion===================//
+
+let valueInNummber = Number(score)
+
+//to check that data type change hua ya nhi//
+
+console.log( typeof valueInNummber);     //yha pe output number means chnage ho gya hiii
+console.log(valueInNummber);    //  yha bhi Nan aaya
+
+//but sometimes   = NaN bhi aata hii output melike in case of 33abc
+
+
+// more example
+let score = true
+
+console.log(typeof score);    // yha pe output string aaya
+
+//================conversion===================//
+
+let valueInNummber = Number(score)
+
+//to check that data type change hua ya nhi//
+
+console.log( typeof valueInNummber);     //yha pe output number means chnage ho gya hiii
+console.log(valueInNummber);    //isme 1 aaya hii mtlb chnage bhi ho gya hii
+
+
+//==================important note=======================//
+/* 
+   "33" => 33
+   "33abc" => NaN
+   true => 1;  false=>0
+*/
+
+let isLogged ="1"
+// to change it number to boolean so let declare another variable
+let boolenisloggedIn = Boolean(isLogged)     //Syntax-- let (any varrable name) = (kisme change krna hii) (previous variable name)
+console.log(boolenisloggedIn);      //Output true aaya mtlb change ho gya
+
+
+//  when we use empty string output --- false
+//when we use string output= true
+
+//=========to change in string==============//
+let somenumber = 33
+
+let stringnumber = String(somenumber)
+console.log(stringnumber);           // output = 33 
+console.log(typeof stringnumber);      // output = string mtlb change ho gya hii
+
+
+
+
+//================================Operations===================================//
